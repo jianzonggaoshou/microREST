19c9469499d7e0247509405e83667e233671cdae
admin <xuzhen0913@163.com>
2018-11-05 21:58:13 +0800

    users

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home_index():
     sql = ''
     api_list = []
     try:
-        sql = 'SELECT version, buildtime, methods, links from apirelease'
+        sql = 'SELECT version, buildtime, methods, links FROM apirelease'
         logging.debug(sql)
         cursor.execute(sql)
         results = cursor.fetchall()
@@ -34,6 +34,40 @@ def home_index():
 
     db.close()
     return jsonify({'api_version': api_list}), 200
+
+
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    return list_users()
+
+
+def list_users():
+    db = pymysql.connect("114.116.74.221", "root", "Huawei12#$", "mydb")
+    cursor = db.cursor()
+    logging.debug("connect mydb success!")
+
+    sql = ''
+    api_list = []
+    try:
+        sql = 'SELECT username, full_name, email, password, id FROM users'
+        logging.debug(sql)
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        logging.debug(results)
+
+        for row in results:
+            a_dict = {}
+            a_dict['username'] = row[0]
+            a_dict['full_name'] = row[1]
+            a_dict['email'] = row[2]
+            a_dict['password'] = row[3]
+            a_dict['id'] = row[4]
+            api_list.append(a_dict)
+    except:
+        logging.debug("%s failed!" % sql)
+
+    db.close()
+    return jsonify({'user_list': api_list}), 200
 
 
 if __name__ == '__main__':
--- lib/mysqlCon.py
+++ lib/mysqlCon.py
@@ -5,10 +5,10 @@ db = pymysql.connect("114.116.74.221", "root", "Huawei12#$", "mydb")
 cursor = db.cursor()
 
 sql = ''
-sql = 'INSERT INTO apirelease (version, buildtime, links, methods) VALUES ' \
-     '("v1", "2017-01-01 10:00:00", "/api/v1/users", "get, post, put, delete"); '
-# sql = 'INSERT INTO users (username, email, password, full_name) VALUES ' \
-#       '("manish123", "manishest@gmail.com", "manish123", "Manish"); '
+# sql = 'INSERT INTO apirelease (version, buildtime, links, methods) VALUES ' \
+#      '("v1", "2017-01-01 10:00:00", "/api/v1/users", "get, post, put, delete"); '
+sql = 'INSERT INTO users (username, email, password, full_name) VALUES ' \
+      '("manish123", "manishest@gmail.com", "manish123", "Manish"); '
 
 try:
     cursor.execute(sql)
